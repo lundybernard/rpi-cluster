43091ad5b4106797fb101d282e8e6ed99c4b7151
lundybernard <lundy.bernard@gmail.com>
2017-09-23 13:12:08 -0500

    GREEN: has_pass filter works

--- ansible/filter_plugins/hash_filter.py
+++ ansible/filter_plugins/hash_filter.py
@@ -1,11 +1,12 @@
 #! /usr/bin/python
 
+import crypt
+
 class FilterModule(object):
     def filters(self):
         return {
             'hash_pas': self.hash_pas,
         }
  
-    def hash_pas(self, a_variable):
-        a_new_variable = a_variable + ' CRAZY NEW FILTER'
-        return a_new_variable
+    def hash_pas(self, password):
+        return crypt.crypt(password, "$6$random_salt")
